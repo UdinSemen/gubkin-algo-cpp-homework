b1e7e1d73c56cc6babef408525259b1b5575c4e3
INuLLiTyI <vovanstalmakov.ru19@gmail.com>
2025-12-15 19:56:25 +0300

    Update flight_organizer.cpp

--- flight_organizer.cpp
+++ flight_organizer.cpp
@@ -6,10 +6,8 @@
 
 using namespace std;
 
-// Вспомогательная функция для парсинга уникального ключа
 void parse_unique_key(const string& key, string& carrier, string& flight_num,
     int& year, int& month, int& day, string& origin, string& dest) {
-    // Формат ключа: AA_123_2024-01-15_JFK_LAX
     size_t pos1 = key.find('_');
     size_t pos2 = key.find('_', pos1 + 1);
     size_t pos3 = key.find('_', pos2 + 1);
@@ -22,7 +20,7 @@ void parse_unique_key(const string& key, string& carrier, string& flight_num,
     size_t dash1 = date.find('-');
     size_t dash2 = date.find('-', dash1 + 1);
     year = stoi(date.substr(0, dash1));
-    month = stoi(date.substr(dash1 + 1, dash2 - dash1 - 1));1
+    month = stoi(date.substr(dash1 + 1, dash2 - dash1 - 1));
     day = stoi(date.substr(dash2 + 1));
 
     origin = key.substr(pos3 + 1, pos4 - pos3 - 1);
@@ -39,11 +37,9 @@ const unordered_set<flight>& FlightOrganizer::get_all_unique_flights() const {
 }
 
 string FlightOrganizer::get_aircraft_key(const flight& f) const {
-    // Парсим из уникального ключа
     string carrier, flight_num, origin, dest;
     int year, month, day;
     parse_unique_key(f.get_unique_key(), carrier, flight_num, year, month, day, origin, dest);
-
     return carrier + "_" + flight_num;
 }
 
@@ -61,13 +57,10 @@ vector<flight> FlightOrganizer::get_flights_by_aircraft(const string& carrier_id
 
 vector<flight> FlightOrganizer::get_flights_by_carrier(const string& carrier_id) const {
     vector<flight> result;
-
     for (const auto& f : unique_flights) {
-        // Парсим carrier_id из уникального ключа
         string key = f.get_unique_key();
         size_t pos = key.find('_');
         string carrier_from_key = key.substr(0, pos);
-
         if (carrier_from_key == carrier_id) {
             result.push_back(f);
         }
@@ -86,18 +79,14 @@ void FlightOrganizer::save_to_csv(const string& filename) const {
         return;
     }
 
-    // Заголовок CSV
     file << "unique_key,carrier,flight_num,year,month,day,origin,dest\n";
 
     for (const auto& f : unique_flights) {
         string key = f.get_unique_key();
-
-        // Парсим данные из уникального ключа
         string carrier, flight_num, origin, dest;
         int year, month, day;
         parse_unique_key(key, carrier, flight_num, year, month, day, origin, dest);
 
-        // Записываем в CSV
         file << key << ","
             << carrier << ","
             << flight_num << ","
@@ -109,15 +98,32 @@ void FlightOrganizer::save_to_csv(const string& filename) const {
     }
 
     file.close();
-    cout << "Saved " << unique_flights.size() << " flights to " << filename << endl;
 }
 
 void FlightOrganizer::organize_by_aircraft() {
     aircraft_to_flights.clear();
-
     for (const auto& f : unique_flights) {
         string key = get_aircraft_key(f);
         aircraft_to_flights[key].push_back(const_cast<flight*>(&f));
     }
+}
 
+void FlightOrganizer::clear_all_structures() {
+    vector_flights.clear();
+    unordered_set_flights.clear();
+    set_flights.clear();
+    unordered_map_flights.clear();
+    map_flights.clear();
+    unordered_multimap_flights.clear();
+    multimap_flights.clear();
+}
+
+void FlightOrganizer::add_flight_to_all(const flight& f) {
+    add_to_container(vector_flights, f);
+    add_to_container(unordered_set_flights, f);
+    add_to_container(set_flights, f);
+    add_to_container(unordered_map_flights, f);
+    add_to_container(map_flights, f);
+    add_to_container(unordered_multimap_flights, f);
+    add_to_container(multimap_flights, f);
 }
